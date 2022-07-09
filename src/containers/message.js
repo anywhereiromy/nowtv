8c6315daf9d61f4ee56e82c64112a2083a897e76
Romy Greenfield <rgreenfield@adaptavist.com>
2022-07-09 12:04:18 +0100

    add timestamp in human readable format

--- src/containers/message.js
+++ src/containers/message.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
 
 const Message = ({ message, member }) => {
   const [showEmail, setShowEmail] = useState(false);
   const { email } = member;
-
+  // TODO: make this timezone friendly
+  // dayjs("2013-11-18 11:55").tz("America/Toronto", true)
+  dayjs.extend(advancedFormat);
+  const parsedTimestamp = dayjs(message.timestamp).format('Do MMM YYYY HH:mm');
   return (
     <div>
       {member.avatar ? (
@@ -23,6 +28,7 @@ const Message = ({ message, member }) => {
         {message.message}
         {showEmail && <a href={`mailto: ${email}`}>{email}</a>}
       </p>
+      <time dateTime={message.timestamp}>{parsedTimestamp}</time>
     </div>
   );
 };
